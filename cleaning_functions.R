6c04a62e9c450ae5d5b924c35f10474a8ae79525
rwjones <redditor.vmsmith@gmail.com>
2017-06-28 20:40:28 -0400

    Minor housekeeping

--- cleaning_functions.R
+++ cleaning_functions.R
@@ -9,8 +9,7 @@ change_dash_to_space <- function(x) chartr("-", " ", x)
 remove_punctuation <- function(x) gsub("[[:punct:]]", "", x)
 remove_digits <- function(x) gsub("[[:digit:]]+", "", x)
 remove_control_characters <- function(x) gsub("[[:cntrl:]]", "", x)
-# Collapse multiple spaces to one space
-collapse_spaces <- function(x) gsub("\\s{2,}", " ", x)
+collapse_spaces <- function(x) gsub("\\s{2,}", " ", x) # Collapse multiple spaces to one space
 
 remove_pictwittercom <- function(x) gsub("pictwittercom\\w+ *"  , "", x, ignore.case = TRUE)
 
@@ -19,8 +18,6 @@ remove_pictwittercom <- function(x) gsub("pictwittercom\\w+ *"  , "", x, ignore.
 
 # Words beginning with "#"
 # remove_hashtag <- function(x) gsub("#\\w+", "", x)
-
-
-remove_miscellaneous <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
+#remove_miscellaneous <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
 
 convert_to_lower <- function(x) tolower(x)
